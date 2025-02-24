b3b3f8e82f6456ac6fe503072d7d826ceb23a34e
Tony <tony.lehuynh@redeye.co>
2025-02-25 00:08:12 +1000

    Useful math methods

---
+++ src/Exercise10_Mathoperations.java
@@ -0,0 +1,25 @@
+public class Exercise10_Mathoperations {
+    public static void main(String[] args){
+
+        System.out.println("Value of pi is " + Math.PI);
+        System.out.println("Value of E is " + Math.E);
+
+
+        double result;
+
+
+        // Useful math methods
+        result = Math.pow(2, 4);
+        result = Math.abs(-5);
+        result = Math.sqrt(9);
+        result = Math.round(3.14);
+        result = Math.ceil(3.14);
+        result = Math.floor(3.14);
+        result = Math.max(10, 20);
+        result = Math.min(10, 20);
+
+
+        System.out.println(result);
+
+    }
+}
