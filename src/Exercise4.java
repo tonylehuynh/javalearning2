f5c7e72e6b42ae9052f0959fae90aae2b03de831
Tony <tony.lehuynh@redeye.co>
2025-02-24 23:09:12 +1000

    4th task - userinput

--- src/Exercise4.java
+++ src/Exercise4.java
@@ -3,6 +3,23 @@ import java.util.Scanner;
 public class Exercise4 {
     public static void main(String[] args){
 
+        // Calculate area of a rectangle
+
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the width: ");
+        width = scanner.nextDouble();
+        System.out.println("Enter the height: ");
+        height = scanner.nextDouble();
+
+        area = width * height;
+        System.out.println("The area of the rectangle is: " + area + "cm²");
+
+        scanner.close();
 
     }
 
