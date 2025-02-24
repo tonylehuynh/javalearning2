0daafb1d52085db7bc2fa4ce552fe99d7cc089bb
Tony <tony.lehuynh@redeye.co>
2025-02-24 23:25:22 +1000

    6th task - arithmetic operators

---
+++ src/Exercise6.java
@@ -0,0 +1,35 @@
+public class Exercise6 {
+    public static void main(String[] args) {
+
+        // Arithmetic Operators
+
+        int  x = 10;
+        int y = 2;
+        int z;
+
+        //z = x + y;
+        //z = x - y;
+        //z = x * y;
+        //z = x / y;
+        z = x % y;
+
+        System.out.println(z);
+
+        //x += y;
+        //x -= y;
+        //x *= y;
+        //x /= y;
+        x %= y;
+        System.out.println(x);
+
+        int a = 1;
+
+        a++;
+        a++;
+        a++;
+        a--;
+
+
+        System.out.println(a);
+    }
+}
